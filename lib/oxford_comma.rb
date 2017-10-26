8db21285dffa56fcd8074bd11c40d6251528bac8
Nolan Hughes <nhughes987@gmail.com>
2017-10-26 20:25:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
  if array.length < 3
    array.join(" and ")
  else
-   last_index = [array.last]
+   last_index = array.last
    array.pop
    array << "and #{last_index}"
    array.join(", ")
